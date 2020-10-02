e5fe098ff0413f5a2fa6ab073b3ab20b22f75ab3
Nirmal <nirmalpandey048@gmail.com>
2020-10-02 19:43:28 +0545

    resolution change for frame

--- 2resolution-change.py
+++ 2resolution-change.py
@@ -27,7 +27,7 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
+    # Our operation on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
    
     # Display the resulting frame
